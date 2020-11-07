d3bf27d138fcd1ad1d6e92d4552bd79d02f85d9b
Chen-Jialin <49650983+Chen-Jialin@users.noreply.github.com>
2020-11-07 14:19:04 +0800

    Lab-2-Note-1 minor correction.

--- Lab-2-Note-1-蔡氏电路简析/ChuasCircuitSim.m
+++ Lab-2-Note-1-蔡氏电路简析/ChuasCircuitSim.m
@@ -6,7 +6,7 @@ R4 = 22000;
 R5 = 22000;
 R6 = 3300;
 V1 = 9;
-V2 = 9;
+V2 = -9;
 L1 = 18e-3;
 C1 = 10e-9;
 C2 = 100e-9;
@@ -14,22 +14,22 @@ global R; % Variable resistance
 R = 1540;
 
 % Negative impedance converter 1 I-V
-Vmax1 = R3 / (R2 + R3) * V2;
-Vmin1 = R3 / (R2 + R3) * (-V1);
+Vmax1 = R3 / (R2 + R3) * V1;
+Vmin1 = R3 / (R2 + R3) * V2;
 V = linspace(2 * Vmin1, 2 * Vmax1, 101);
 I = (V <= Vmax1) .* (V >= Vmin1) .* (- R2 / (R3 * R1) * V)...
     + (V > Vmax1) .* (V - V1) / R1...
-    + (V < Vmin1) .* (V + V2) / R1;
+    + (V < Vmin1) .* (V - V2) / R1;
 figure(1)
 plot(V, 1e3 * I, 'k-')
 hold on
 
 % Negative impedance converter 2 I-V
-Vmax2 = R6 / (R5 + R6) * V2;
-Vmin2 = R6 / (R5 + R6) * (-V1);
+Vmax2 = R6 / (R5 + R6) * V1;
+Vmin2 = R6 / (R5 + R6) * V2;
 I = (V <= Vmax2) .* (V >= Vmin2) .* (- R5 / (R6 * R4) * V)...
     + (V > Vmax2) .* (V - V1) / R4...
-    + (V < Vmin2) .* (V + V2) / R4;
+    + (V < Vmin2) .* (V - V2) / R4;
 plot(V, 1e3 * I, 'k--')
 
 grid on
@@ -40,10 +40,10 @@ legend('蔡氏二极管1（含R1,R2,R3）', '蔡氏二极管2（含R4,R5,R6）', 'fontsize', 12)
 % Chua's Diode I-V
 I = (V <= Vmax1) .* (V >= Vmin1) .* (- R2 / (R3 * R1) * V)...
     + (V > Vmax1) .* (V - V1) / R1...
-    + (V < Vmin1) .* (V + V2) / R1...
+    + (V < Vmin1) .* (V - V2) / R1...
     + (V <= Vmax2) .* (V >= Vmin2) .* (- R5 / (R6 * R4) * V)...
     + (V > Vmax2) .* (V - V1) / R4...
-    + (V < Vmin2) .* (V + V2) / R4;
+    + (V < Vmin2) .* (V - V2) / R4;
 figure(2)
 plot(V, 1e3 * I, 'k', 'linewidth', 2)
 grid on
@@ -54,23 +54,23 @@ ylabel('I / mA', 'fontsize', 16)
 % Simulation time range
 t_span = [0, 0.05];
 % Initial condition
-y_0 = [0; 0.01; -0.02];
-% y = [I_L; V_C1; V_C2]^T
+y_0 = [0.01; -0.02; 0];
+% y = [V_C1; V_C2; I_L]^T
 
 % R = 1538;
 % y_0 = [0; 0.01; -0.01];
 % for i = 0:5
 %     [t, y] = ode45(@ChuaCircuitSimFunc, t_span, y_0);
 %     figure(3 + i)
-%     plot(y(100:end,2), y(100:end,3), '-')
+%     plot(y(100:end, 1), y(100:end, 2), '-')
 %     R = R + 1;
 % end
 
 R = 1540;
-y = [0; 0.01; -0.02];
+y_0 = [0.01; -0.02; 0];
 for i = 0:5
-    [t, y] = ode45(@ChuaCircuitSimFunc, t_span, y_0);
+    [t, y] = ode45(@ChuasCircuitSimFunc, t_span, y_0);
     figure(3 + i)
-    plot(y(:,2), y(:,3), '-')
-    y_0(3) = y_0(3) + 0.01;
+    plot(y(:, 1), y(:, 2), '-')
+    y_0(2) = y_0(2) + 0.01;
 end
---
+++ Lab-2-Note-1-蔡氏电路简析/ChuasCircuitSimFunc.m
@@ -0,0 +1,26 @@
+function dy = ChuasCircuitSimFunc(t, y)
+    R1 = 220;
+    R2 = 220;
+    R3 = 2200;
+    R4 = 22000;
+    R5 = 22000;
+    R6 = 3300;
+    V1 = 9;
+    V2 = -9;
+    L = 18e-3;
+    C1 = 10e-9;
+    C2 = 100e-9;
+    global R;
+    Vmax1 = R3 / (R2 + R3) * V1;
+    Vmin1 = R3 / (R2 + R3) * V2;
+    Vmax2 = R6 / (R5 + R6) * V1;
+    Vmin2 = R6 / (R5 + R6) * V2;
+    I_R = @(V) (V <= Vmax1) .* (V >= Vmin1) .* (- R2 / (R3 * R1) * V)...
+    + (V > Vmax1) .* (V - V1) / R1...
+    + (V < Vmin1) .* (V - V2) / R1...
+    + (V <= Vmax2) .* (V >= Vmin2) .* (- R5 / (R6 * R4) * V)...
+    + (V > Vmax2) .* (V - V1) / R4...
+    + (V < Vmin2) .* (V - V2) / R4;
+    dy(1,1) = - (I_R(y(1, 1)) - (y(2, 1) - y(1, 1)) / R) / C1;
+    dy(2,1) = - ((y(2, 1) - y(1, 1)) / R - y(3, 1)) / C2;
+    dy(3,1) = - y(2, 1) / L;
